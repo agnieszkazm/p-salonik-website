a28fb83f716158d00cbd4c07bb008094bade673c
agnieszka <agnieszka.rydz@gmail.com>
2017-03-01 00:09:30 +0100

    added animation

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+	
+	// Project configuration.
+	grunt.initConfig({
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'style.css': 'style.sass'
+				}
+			}
+		},
+		/*watch: {
+			scripts: {
+				files: ['*.sass'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				},
+			}
+		},*/
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'img/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'img/build/'
+				}]
+			}
+		}
+	});
+		// Load the plugins tasks
+		grunt.loadNpmTasks('grunt-sass');
+		//grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-contrib-imagemin');
+		// Default task(s).
+		grunt.registerTask('default', ['sass', /*'watch',*/ 'imagemin']); 
+		};
